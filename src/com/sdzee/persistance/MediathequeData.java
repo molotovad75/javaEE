468b9f54c273115c24769111ba5ad50d8aea521f
Adrien <adrien.lac@tutanota.com>
2020-03-04 22:46:20 +0100

    Essais pour coder la méthode nouveauDocument(int args0, Object... arg1)

--- src/com/sdzee/persistance/MediathequeData.java
+++ src/com/sdzee/persistance/MediathequeData.java
@@ -1,33 +1,45 @@
 package com.sdzee.persistance;
-import java.util.List;
+import java.util.ArrayList;
 import mediatek2020.PersistentMediatheque;
 import mediatek2020.items.Document;
 import mediatek2020.items.Utilisateur;
 
 public class MediathequeData implements PersistentMediatheque{
+	
+	 ArrayList<Document> documents=new ArrayList<Document>();
 
+	public String toString() {
+		return documents.toString(); 
+	}
+	
 	@Override
 	public Utilisateur getUser(String login, String password) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public Document getDocument(int arg0) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
-	@Override
+	@Override //Object... arg1  est équivalent à un tableau (Object[] arg1) La taille de ce tableau est unconnue.
 	public void nouveauDocument(int arg0, Object... arg1) {
-		// TODO Auto-generated method stub
+		//Peut être que arg0 est le nombre de documents que l'on souhaite mettre dans la bibliothèque.
+		arg1=documents.toArray(arg1); //Converti le tableau en arraylist.
+		
+		arg0=arg1.length;
+		for (int i = 0; i < arg1.length; i++) {
+			
+		}
 		
 	}
 
 	@Override
-	public List<Document> tousLesDocuments() {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<Document> tousLesDocuments() {
+
+		return documents;
 	}
 	
 }
