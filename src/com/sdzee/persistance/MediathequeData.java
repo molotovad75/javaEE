98fb0d2788bb772ab09259dc563e22bcc47b78da
Adrien <adrien.lac@tutanota.com>
2020-03-09 11:20:29 +0100

    Ajout d'un package.jar

--- src/com/sdzee/persistance/MediathequeData.java
+++ src/com/sdzee/persistance/MediathequeData.java
@@ -1,11 +1,14 @@
 package com.sdzee.persistance;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import mediatek2020.PersistentMediatheque;
 import mediatek2020.items.Document;
 import mediatek2020.items.Utilisateur;
 
 public class MediathequeData implements PersistentMediatheque{
-	
+	 private Connection connexion;
 	 ArrayList<Document> documents=new ArrayList<Document>();
 
 	public String toString() {
@@ -14,7 +17,14 @@ public class MediathequeData implements PersistentMediatheque{
 	
 	@Override
 	public Utilisateur getUser(String login, String password) {
-		String requeteSQL="SELECT u.name u.email FROM utilisateur AS u WHERE u.login="+login+" AND u.mdp="+password+" ";
+		String requeteSQL="SELECT u.name u.email FROM utilisateur AS u WHERE u.login="+login+" AND u.mdp="+password+"; ";
+		try {
+			
+		} catch (Exception e) {
+			
+		}finally {
+			
+		}
 		return null;
 	}
 
@@ -22,7 +32,7 @@ public class MediathequeData implements PersistentMediatheque{
 	public Document getDocument(int arg0) {
 		//int arg0 siginifie probablement le numéro du document.
 		//Cd DVD ou livre peuvent être des documens.
-		String requeteSQL="SELECT l.titre_livre,d.titre_dvd,c.titre_cd FROM livre AS l, dvd AS d, cd AS c WHERE l.idlivre="+arg0+"";
+		String requeteSQL="SELECT l.titre_livre,d.titre_dvd,c.titre_cd FROM livre AS l, dvd AS d, cd AS c WHERE l.idlivre="+arg0+" OR d.iddvd="+arg0+" OR c.idcd="+arg0+"; ";
 		
 		return null;
 	}
@@ -47,4 +57,18 @@ public class MediathequeData implements PersistentMediatheque{
 		return documents;
 	}
 	
+	private void loadDatabase() {
+        // Chargement du driver
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+               e.getMessage();
+        }
+
+        try {
+            connexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/pweb19_leroux","root","");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
